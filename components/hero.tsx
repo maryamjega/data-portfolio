676f91ab493b24dbb20fc9db388e2a110316cdef
maryamjega <142058432+maryamjega@users.noreply.github.com>
2026-08-01 21:31:43 +0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
           bottom: -10,
           left: -10,
           fontFamily: "var(--font-archivo-black, 'Archivo Black'), sans-serif",
-          fontWeight: 900,
+          fontWeight: 1000,
           fontSize: "clamp(100px,14vw,120px)",
           color: "transparent",
           WebkitTextStroke: "1px rgba(17,16,16,0.05)",
